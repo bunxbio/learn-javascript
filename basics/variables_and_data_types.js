500523c9707a3b036a4ee9fd12575f71129fbc23
bunxbio <biokenfigumilang@gmail.com>
2026-05-17 01:02:54 +0700

    variables and data types

---
+++ basics/variables_and_data_types.js
@@ -0,0 +1,36 @@
+// VARIABLES
+let firstName = 'Bio';
+let lastName = 'Gumilang';
+
+const DATE_OF_BIRTH = '1997.03.13';
+
+
+// DATA TYPES
+let favoriteFood = 'Pizza'; //string
+
+let numberOfFood = 20; //number
+let pi = 3.14;
+
+let veryLargeNumber = 123345457123n; //BigInt
+
+let learning = true; //boolean
+
+let hungry; //undefined
+
+favoriteFood = null; //null
+
+const uniqueKey = Symbol(); //symbol
+
+//object
+let course = {
+    name: 'JavaScript course',
+    hours: 1,
+}
+
+let products = ['shirts', 'pants']
+
+function sayHi(name) {
+    return `Hi ${name}`;
+}
+
+console.log(sayHi(firstName));
